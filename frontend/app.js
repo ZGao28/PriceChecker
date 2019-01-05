64c323a851dabf597d6877d75b87f5a394cbcf02
Zi Gao <zi.s.gao@gmail.com>
2019-01-05 15:31:17 -0500

    set up react

---
+++ frontend/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/stylesheet.scss';
+
+const sample = (
+    <h1>
+        Hello World
+    </h1>
+);
+
+ReactDOM.render(sample, document.getElementById('app'));
